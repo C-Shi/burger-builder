5023dbac4398c15fa92b10dd5c44a737b3e1290c
C-Shi <cshi@ualberta.ca>
2019-01-05 11:38:31 +0800

    form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your name',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your street',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       zipCode: {
         elementType: 'input',
@@ -31,7 +39,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Zipcode',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -39,7 +52,11 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Country',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: 'input',
@@ -47,7 +64,11 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'example@email.com',
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false
       },
     },
     ingredients: {},
@@ -79,12 +100,27 @@ class ContactData extends Component {
     })
   }
 
+  checkValidity = (value, rules) => {
+    let isValid = true;
+    if(rules.required){
+      isValid = value.trim() !== '';
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength
+    }
+
+    return isValid;
+  }
+
   inputChangeHandler = (event, inputIdentifier) => {
     const updatedOrderForm = Object.assign({}, this.state.orderForm);
     const updatedFormElement = updatedOrderForm[inputIdentifier];
     // using object.assign to create a copy but not reference of the original object
     // update formElement value to real value
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    console.log(updatedFormElement)
     this.setState({orderForm: updatedOrderForm})
   }
 
@@ -108,7 +144,7 @@ class ContactData extends Component {
             value={formElement.config.value} 
             changed={(event) => this.inputChangeHandler(event, formElement.id)}/>
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>Button</Button>
+        <Button btnType="Success" clicked={this.orderHandler}>Place Order</Button>
       </form>
     );
     if (this.state.loading) {
